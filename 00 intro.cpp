9f92e7947fc0527efa3768f667907eff81ba6f95
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-06 12:28:07 +0530

    Create 00 intro.cpp

---
+++ 00 intro.cpp
@@ -0,0 +1,24 @@
+used for 2 types of operations:
+1. PREFIX operations (sum, poroduct, xor, or, etc)
+2. update a value
+
+
+represented in form of an array
+
+
+time complexity: O(N* logN)  .... operations take logN time, but pre-processing takes more time
+space complexity: O(N)
+
+
+its almost similar like segment tree, but:
+1. here the tree (represented as an array) node can have more than 2 children
+2. its FASTER than seg tree
+
+
+
+example we want sum in range->
+
+sum(l,r) = sum(0,r) - sum(0,l-1)
+seg tree    fenwick    fenwick
+
+so same example cna be done by both types of trees
