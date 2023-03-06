bf9250d9422213a1ee7770a616c423c33fd50eb2
veduKaGit <104839104+veduKaGit@users.noreply.github.com>
2023-03-07 01:18:19 +0530

    Update 04 update.cpp

--- 04 update.cpp
+++ 04 update.cpp
@@ -1,13 +1,19 @@
-#include <iostream> 
+// in this file, we are considering that the Binary Index Tree array is ALREADY constructed
+// we just want to implement the update function
+// update(i) updates the original array at index i, and also updates the fenwick tree
 
-using namespace std; 
+// time complexity: O(log N)
 
- 
+// like the previous files=> here also => consider => operation of prefix sum (ie. fenwick tree ka getSum(i) returns sum of arr elements from range [0,i])
+
+// fenwick tree can have some other operation as well(eg prefix xor, prefix or, etc).....we are just considering an example here (of prefix sum)
+
+
 
 void updateBIT(int BITree[], int n, int index, int val) 
 { 
     
-    index = index + 1; 
+    index = index + 1;   //
   
      
     while (index <= n) 
@@ -20,35 +26,3 @@ void updateBIT(int BITree[], int n, int index, int val)
     } 
 }
  
-int *constructBITree(int arr[], int n) 
-{ 
-	
-	int *BITree = new int[n+1]; 
-	for (int i=1; i<=n; i++) 
-		BITree[i] = 0; 
-
-	 
-	for (int i=0; i<n; i++) 
-		updateBIT(BITree, n, i, arr[i]); 
-
-	
-
-	return BITree; 
-} 
-
-
- 
-int main() 
-{ 
-	int freq[] = {10, 20, 30, 40, 50, 60, 70, 80, 90}; 
-	int n = sizeof(freq)/sizeof(freq[0]); 
-	int *BITree = constructBITree(freq, n); 
-	
-	updateBIT(BITree, n, 2, 35); 
-		
-	
-
-	
-	
-	return 0; 
-} 
